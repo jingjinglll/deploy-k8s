f27677bdc61804f6e051644498a6f1bbcf3b8ab8
jingjinglll <jingjinglll@gmail.com>
2018-11-21 18:00:25 +0800

    export dashboard only

--- export_longevity.sh
+++ export_longevity.sh
@@ -14,7 +14,7 @@ jmeter_po=`kubectl get po -n $namespace| grep jmeter | awk '{print $1}'`
 kubectl cp ./jmeter/generate_report.sh $jmeter_po:/bin -n $namespace
 kubectl exec $jmeter_po -n $namespace -- ./bin/generate_report.sh
 
-kubectl cp $jmeter_po:output/ output/ -n $namespace
+kubectl cp $jmeter_po:output/dashboard output/dashboard -n $namespace
 echo "Longevity reports are saved to ./output"
 
 kubectl exec $jmeter_po -n $namespace -- rm -rf /output/dashboard
