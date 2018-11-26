fb811d22f6e2324f5f8003720534d28b1ce30f56
jingjinglll <jingjinglll@gmail.com>
2018-11-26 18:07:07 +0800

    fix jmeter csv

--- export_longevity.sh
+++ export_longevity.sh
@@ -9,15 +9,41 @@ do
   esac
 done
 
-jmeter_po=`kubectl get po -n $namespace| grep jmeter | awk '{print $1}'`
+jmeter_po=`kubectl get po -n $namespace| grep jmeter | grep Running | awk '{print $1}'`
 
 kubectl cp ./jmeter/generate_report.sh $jmeter_po:/bin -n $namespace
 kubectl exec $jmeter_po -n $namespace -- ./bin/generate_report.sh
 
-kubectl cp $jmeter_po:output/dashboard output/dashboard -n $namespace
-echo "Longevity reports are saved to ./output"
+if [ -e output/dashboard ]; then
+  rm -rf output/dashboard
+fi
 
-kubectl exec $jmeter_po -n $namespace -- rm -rf /output/dashboard
+kubectl cp $jmeter_po:output/dashboard output/dashboard -n $namespace
+
+if [ $? -eq 0 ]; then
+  echo "Longevity reports are saved to ./output"
+  kubectl exec $jmeter_po -n $namespace -- rm -rf /output/dashboard
+else
+  echo "Failed in export reports in container, try to generate reports locally"
+  kubectl cp $jmeter_po:output/log.csv output/log.csv -n $namespace
+
+  cat  output/log.csv  | awk 'FPAT="([^,]+)|(\"[^\"]+\")"{x=$0;while((gsub(/\"/,"\"",x)%2)!=0){getline;x=x " ";x=x $0};$0=x;print}' | head -n -1 >> output/temp.csv
+  cat  output/temp.csv  | awk -F',' 'NF == 17' > output/temp2.csv
+
+  if [ ! -e apache-jmeter-5.0 ]; then
+    wget http://mirrors.advancedhosters.com/apache/jmeter/binaries/apache-jmeter-5.0.tgz
+    tar xzvf apache-jmeter-5.0.tgz
+    rm apache-jmeter-5.0.tgz
+  fi
+  
+  apache-jmeter-5.0/bin/jmeter -g output/temp2.csv -o output/dashboard
+  if [ $? -eq 0 ]; then
+  	echo "Longevity reports are saved to ./output"
+    rm output/temp.csv output/temp2.csv output/log.csv
+  fi
+fi
+
+
 
 
 
--- jmeter/generate_report.sh
+++ jmeter/generate_report.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 head -n -1 /output/log.csv > /temp.csv
-/apache-jmeter-5.0/bin/jmeter -g /temp.csv -o /output/dashboard
-rm /temp.csv
+cat  /output/log.csv  | awk 'FPAT="([^,]+)|(\"[^\"]+\")"{x=$0;while((gsub(/\"/,"\"",x)%2)!=0){getline;x=x " ";x=x $0};$0=x;print}' | head -n -1 >> /temp.csv
+cat  /temp.csv  | awk -F',' 'NF == 17' > /temp2.csv
+/apache-jmeter-5.0/bin/jmeter -g /temp2.csv -o /output/dashboard
+rm /temp.csv /temp2.csv
